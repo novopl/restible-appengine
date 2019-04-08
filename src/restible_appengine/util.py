760a673eaf56181d4d29b13120f7cfa990853608
Mateusz Klos <novopl@gmail.com>
2019-04-08 10:04:02 +0100

    Update to restible 0.11.1

--- src/restible_appengine/util.py
+++ src/restible_appengine/util.py
@@ -3,11 +3,23 @@
 from __future__ import absolute_import, unicode_literals
 
 # stdlib imports
-from typing import Any, Dict, List, Text, Type
+from typing import Any, Dict, Text, Type
 
 # 3rd party imports
 from six import iteritems
-from google.appengine.ext import ndb
+from google.appengine.ext import ndb    # pylint: disable=wrong-import-order
+from restible.util import QsFilter
+
+
+OP_MAP = {
+    'eq': '__eq__',
+    'ne': '__ne__',
+    'lt': '__lt__',
+    'gt': '__gt__',
+    'le': '__le__',
+    'ge': '__ge__',
+    'in': 'IN',
+}
 
 
 def ndb_query_from_values(model_cls, filters):
@@ -43,39 +55,10 @@ def ndb_query_from_values(model_cls, filters):
         >>> person = ndb_query_from_values(Person, filters).get()
 
     """
-    ndb_filters = _parse_qs_filter(model_cls, filters)
-
-    return model_cls.query().filter(*ndb_filters)
-
-
-def _parse_qs_filter(model_cls, qs_params):
-    # type: (Type[ndb.Model], Dict[Text, Text]) -> List[Any]
-    op_map = {
-        'eq': lambda mdl, name, value: getattr(mdl, name) == value,
-        'ne': lambda mdl, name, value: getattr(mdl, name) != value,
-        'lt': lambda mdl, name, value: getattr(mdl, name) < value,
-        'gt': lambda mdl, name, value: getattr(mdl, name) > value,
-        'lte': lambda mdl, name, value: getattr(mdl, name) <= value,
-        'gte': lambda mdl, name, value: getattr(mdl, name) >= value,
-        'in': lambda mdl, name, value: getattr(mdl, name).IN(value),
-    }
-
-    ndb_filters = []
-    for flt, value in iteritems(qs_params):
-        parts = flt.rsplit('__', 1)
-        if len(parts) == 2:
-            name, op = parts
-        else:
-            op = 'eq'
-            name = parts[0]
-
-        op_fn = op_map.get(op)
-        if not op_fn:
-            raise ValueError("Invalid OP: {} in '{}'".format(op, flt))
-
-        ndb_filters.append(op_fn(model_cls, name, value))
-    return ndb_filters
+    filters = [QsFilter.build(OP_MAP, k, v) for k, v in iteritems(filters)]
+    ndb_filters = [flt(model_cls) for flt in filters]
+    return model_cls.query(*ndb_filters)
 
 
 # Used only in type hint comments.
-del Any, Dict, List, Text, Type, ndb
+del Any, Dict, Text, Type, ndb
